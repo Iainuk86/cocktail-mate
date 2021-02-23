8211e59bc866c1fddc8fb418e72eba0f0afb482a
Iainuk86 <iainuk1986@gmail.com>
2021-02-23 20:03:47 +1100

    Added WebConfig and Cocktail repository

---
+++ src/main/java/org/iainuk/cocktailapp/controller/CocktailController.java
@@ -0,0 +1,13 @@
+package org.iainuk.cocktailapp.controller;
+
+import org.iainuk.cocktailapp.dao.CocktailRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CocktailController {
+
+    @Autowired
+    private CocktailRepo cocktailRepo;
+}
--- src/main/java/org/iainuk/cocktailapp/dao/CocktailRepo.java
+++ src/main/java/org/iainuk/cocktailapp/dao/CocktailRepo.java
@@ -2,9 +2,11 @@ package org.iainuk.cocktailapp.dao;
 
 import org.iainuk.cocktailapp.Cocktail;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CocktailRepo extends CrudRepository<Cocktail, Integer> {
 
     List<Cocktail> findAllByOrderByName();
