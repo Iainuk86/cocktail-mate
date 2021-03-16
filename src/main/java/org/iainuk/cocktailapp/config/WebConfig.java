dd19c6f89e3059721084c67c6fc65441e08be87f
Iainuk86 <iainuk1986@gmail.com>
2021-03-16 12:33:12 +1100

    Some refactoring. Almost ready to deploy

--- src/main/java/org/iainuk/cocktailapp/config/WebConfig.java
+++ src/main/java/org/iainuk/cocktailapp/config/WebConfig.java
@@ -21,6 +21,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/cocktails").setViewName("cocktails");
     }
 }
